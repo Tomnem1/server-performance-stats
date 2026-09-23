cc07e6ae39796c6b6a1fb2526d27035d67b3171c
Tomnem1 <tominemcik@gmail.com>
2026-09-23 12:55:57 +0200

    Add top 5 processes sorted by CPU and MEM usage to server-stats.sh

--- server-stats.sh
+++ server-stats.sh
@@ -3,3 +3,7 @@ echo "#### MEMORY USAGE ####"
 free | grep 'Mem' | awk '{printf "Free: %.2fGi  Used: %.2fGi  Usage: %.2f%%\n", $4/1048576, $3/1048576, $3/$2*100}'
 echo "#### DISK USAGE ####"
 df -h | grep '/$' | awk '{print "Used: " $3,"Free: " $4,"Usage: " $5}'
+echo "#### TOP 5 PROCESSES BY CPU USAGE ####"
+ps -eo pid,%cpu,%mem,comm --sort=-%cpu | head -n 6
+echo "#### TOP 5 PROCESSES BY MEMORY USAGE ####"
+ps -eo pid,%cpu,%mem,comm --sort=-%mem | head -n 6
