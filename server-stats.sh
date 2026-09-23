648da1bdb7f16e08207dda0b29fd640bdf00ecc3
Tomnem1 <tominemcik@gmail.com>
2026-09-23 12:31:49 +0200

    Add memory usage stat to server-stats.sh

--- server-stats.sh
+++ server-stats.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
+echo "#### MEMORY USAGE ####"
+free | grep 'Mem' | awk '{printf "Free: %.2fGi  Used: %.2fGi  Usage: %.2f%%\n", $4/1048576, $3/1048576, $3/$2*100}'
+echo "#### DISK USAGE ####"
 df -h | grep '/$' | awk '{print "Used: " $3,"Free: " $4,"Usage: " $5}'
